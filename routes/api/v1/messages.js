4f7f8a775d8afa6d57e188a4b566bce1bd0d6117
Thibo <thibovaningelgem@gmail.com>
2024-10-04 13:57:16 +0200

    added GET /api/messages/:id

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -22,17 +22,21 @@ router.get("/", (req, res, next) => {
         }
     });
 });
+//geef één bericht terug op basis van de id
+//je kan hier de array keys 0, 1, ... gebruiken of gewoon één bericht terugsturen naar keuze
+//als een bericht niet gevonden kan worden kan je status code 404 teruggeven ipv 200
+
+router.get("/:id", (req, res, next) => {
+    res.status(200).json({ 
+        status: "success",
+        message:  "GET message",
+        data: {
+            "message": {
+                id: 1,
+            }
+        }
+    });
+}
+);
 
 module.exports = router;
-
-// fix render not working
-// var express = require('express');
-// var router = express.Router();
-//
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = router;
-
