01c05e7f736886bcf080f5b3554c5ecab573fefa
Thibo <thibovaningelgem@gmail.com>
2024-10-04 12:39:53 +0200

    feat: add messages

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -3,11 +3,22 @@ var router = express.Router();
 
 // Define the GET endpoint for "/"
 router.get("/", (req, res, next) => {
-    res.json({ 
+    res.status(200).json({ 
         status: "success",
         message:  "GET messages",
         data: {
-            "messages": []
+            "messages": [
+                {
+                    "id": 1,
+                    "user": "user1",
+                    "message": "message1"
+                },
+                {
+                    "id": 2,
+                    "user": "user2",
+                    "message": "message2"
+                }
+            ]
         }
     });
 });
